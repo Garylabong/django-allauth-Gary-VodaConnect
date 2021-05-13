b9beca09d54c2ecebf489d5795a943320b2e3a8e
psalmsglobal <psalmsglobal.0998@gmail.com>
2021-05-13 14:06:31 -0500

    Updated

--- OrderRequest/urls.py
+++ OrderRequest/urls.py
@@ -4,4 +4,9 @@ from OrderRequest.views import *
 app_name = "order_req"
 urlpatterns = [
     path("order/request/", OrderRequestListView.as_view(), name="order_request"),
+    path(
+        "order/request/add/",
+        OrderRequestAddView.as_view(),
+        name="orderrequest_add",
+    ),
 ]
--- OrderRequest/views.py
+++ OrderRequest/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from OrderRequest.models import *
 from django.views.generic import (
     TemplateView,
@@ -12,3 +13,30 @@ from django.views.generic import (
 class OrderRequestListView(ListView):
     model = OrderRequest
     context_object_name = "OrderRequest"
+
+
+class OrderRequestAddView(CreateView):
+    model = OrderRequest
+    fields = (
+        "date_request",
+        "plan_type",
+        "company_name",
+        "address",
+        "preferred_code",
+        "phone_number",
+        "fax",
+        "request",
+        "category_request",
+        "email",
+        "order_status",
+        "notes",
+    )
+    context_object_name = "add"
+    template_name = "OrderRequest/orderrequest_add.html"
+
+    def form_valid(self, form):
+        orderrequest = form.save(commit=False)
+        orderrequest.user = self.request.user
+        orderrequest.save()
+        messages.success(self.request, "You have been Created a Details!")
+        return redirect("order_req:order_request")
